688b37f5ce1bf9cda96b6a8afeccf2cf99525052
Arevass <glenn278@hotmail.com>
2015-09-30 15:43:18 +0000

    Answers must be surrounded with ' " "

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -1,5 +1,18 @@
-pal = input()
+#pal = input()
 
-if str(pal) == str(pal)[::-1]:
-  print "true"
-else: print "false"
+#if str(pal) == str(pal)[::-1]:
+#  print "true"
+#else: print "false"
+
+def palindrome(pal):
+  if len(pal) <= 1:
+    return True
+  else:
+    if pal[0] != pal[len(pal)-1]:
+      return False
+    else:
+      return palindrome(pal[1:len(pal)-1])
+
+pal = input("Enter a word! ")
+
+print(palindrome(pal))
